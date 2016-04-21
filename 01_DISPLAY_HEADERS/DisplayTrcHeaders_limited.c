65a49c0ec4cf9d3958302d57c9a14d23c307d8d0
jaxbal <ovallesa@me.com>
2016-04-20 21:00:48 -0430

    Making DisplayTrcHeaders_limited.c to show reversed list

--- 01_DISPLAY_HEADERS/DisplayTrcHeaders_limited.c
+++ 01_DISPLAY_HEADERS/DisplayTrcHeaders_limited.c
@@ -33,7 +33,9 @@ int main(int argc, char **argv) {
 
   
   if (argv[1] == NULL){
-    printf("Syntax: >> DisplayTrcHeaders filename TraceInic TraceEnd\n");
+    printf("\n\n Syntax: >> DisplayTrcHeaders filename TraceInic TraceEnd\n");
+    printf("\n If you want to see the reversed list then use: \n");
+    printf("\n Syntax: >> DisplayTrcHeaders filename -NumberOfTraces 0\n\n");
   }
   else{
     filename = argv[1];
@@ -55,51 +57,105 @@ int main(int argc, char **argv) {
     fseek( fp, 3600, SEEK_SET );
     int byteOffset = 0;
     printf("Samples: %d\n", nm);
- 
-    int trc = 1;
-    int seq_line;
-    while ( trc < strtrc){
-      fseek( fp, (240 + nm * 4), SEEK_CUR );
-      trc++;
-    }
+
+    int seq_line; 
+
+    if (strtrc > 0){
+
+      int trc = 1;
+      while ( trc < strtrc){
+        fseek( fp, (240 + nm * 4), SEEK_CUR );
+        trc++;
+      }
     
-    printf("seq_line, seq_file, cdp, cdp_seq, cdp_fold, offset, cdpx, cdpy, iln, xln, datumProcTime, datumProcMeter, datumFloatMeter\n");
+      printf("seq_line, seq_file, cdp, cdp_seq, cdp_fold, offset, cdpx, cdpy, iln, xln, datumProcTime, datumProcMeter, datumFloatMeter\n");
 
-    while (trc < maxtrc + 1 ){ //iterar hasta el maximo numero de traces
+      while (trc < maxtrc + 1 ){ //iterar hasta el maximo numero de traces
  
-      unsigned int seq_line = getint( byteOffset );
-      byteOffset = 0;
-      unsigned int seq_file = getint( byteOffset );
-      byteOffset = 12;
-      unsigned int cdp = getint( byteOffset );
-      byteOffset = 0;
-      unsigned int cdp_seq = getint( byteOffset );
-      byteOffset = 4;
-      short cdp_fold = getshort( byteOffset );
-      byteOffset = 2;
-      unsigned int offset = getint( byteOffset );
-      byteOffset = 140;
-      unsigned int cdpx = getint( byteOffset );
-      byteOffset = 0;
-      unsigned int cdpy = getint( byteOffset );
-      byteOffset = 0;
-      unsigned int iln = getint( byteOffset );
-      byteOffset = 0;
-      unsigned int xln = getint( byteOffset );
-      byteOffset = 0;
-      short datum_pt = getshort( byteOffset ); //Datum de procesamiento en tiempo
-      byteOffset = 0;
-      short datum_p = getshort( byteOffset );  //Datum de procesamiento en metros
-      byteOffset = 20;
-      short datum_f = getshort( byteOffset );  //Datum flotante en metros
+        unsigned int seq_line = getint( byteOffset );
+        byteOffset = 0;
+        unsigned int seq_file = getint( byteOffset );
+        byteOffset = 12;
+        unsigned int cdp = getint( byteOffset );
+        byteOffset = 0;
+        unsigned int cdp_seq = getint( byteOffset );
+        byteOffset = 4;
+        short cdp_fold = getshort( byteOffset );
+        byteOffset = 2;
+        unsigned int offset = getint( byteOffset );
+        byteOffset = 140;
+        unsigned int cdpx = getint( byteOffset );
+        byteOffset = 0;
+        unsigned int cdpy = getint( byteOffset );
+        byteOffset = 0;
+        unsigned int iln = getint( byteOffset );
+        byteOffset = 0;
+        unsigned int xln = getint( byteOffset );
+        byteOffset = 0;
+        short datum_pt = getshort( byteOffset ); //Datum de procesamiento en tiempo
+        byteOffset = 0;
+        short datum_p = getshort( byteOffset );  //Datum de procesamiento en metros
+        byteOffset = 20;
+        short datum_f = getshort( byteOffset );  //Datum flotante en metros
 
-      printf("%d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\n", 
-  	     seq_line, seq_file, cdp, cdp_seq, cdp_fold,
-	     offset, cdpx, cdpy, iln, xln, datum_pt, datum_p, datum_f, feof(fp));
-      trc++;
-      byteOffset = (240-222)+ nm * 4;
-      fseek( fp, (240-222)+ nm * 4, SEEK_CUR );
-      byteOffset = 0;
+        printf("%d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\n", 
+   	       seq_line, seq_file, cdp, cdp_seq, cdp_fold,
+	       offset, cdpx, cdpy, iln, xln, datum_pt, datum_p, datum_f, feof(fp));
+        trc++;
+        byteOffset = (240-222)+ nm * 4;
+        fseek( fp, (240-222)+ nm * 4, SEEK_CUR );
+        byteOffset = 0;
+      }
+    }
+      // REVERSE reading of SGY file
+    else{
+      fseek( fp, 0, SEEK_END );
+      byteOffset = -1*(240 + nm * 4);
+      int trc = 1;
+      /*
+      while ( trc < maxtrc){
+        fseek( fp, -1* (240 + nm * 4), SEEK_END )
+        trc++;
+      }
+      */
+      printf("seq_line, seq_file, cdp, cdp_seq, cdp_fold, offset, cdpx, cdpy, iln, xln, datumProcTime, datumProcMeter, datumFloatMeter\n");
+
+      while (trc <= abs(strtrc) ){ //iterar hasta el maximo numero de traces
+ 
+        unsigned int seq_line = getint( byteOffset );
+        byteOffset = 0;
+        unsigned int seq_file = getint( byteOffset );
+        byteOffset = 12;
+        unsigned int cdp = getint( byteOffset );
+        byteOffset = 0;
+        unsigned int cdp_seq = getint( byteOffset );
+        byteOffset = 4;
+        short cdp_fold = getshort( byteOffset );
+        byteOffset = 2;
+        unsigned int offset = getint( byteOffset );
+        byteOffset = 140;
+        unsigned int cdpx = getint( byteOffset );
+        byteOffset = 0;
+        unsigned int cdpy = getint( byteOffset );
+        byteOffset = 0;
+        unsigned int iln = getint( byteOffset );
+        byteOffset = 0;
+        unsigned int xln = getint( byteOffset );
+        byteOffset = 0;
+        short datum_pt = getshort( byteOffset ); //Datum de procesamiento en tiempo
+        byteOffset = 0;
+        short datum_p = getshort( byteOffset );  //Datum de procesamiento en metros
+        byteOffset = 20;
+        short datum_f = getshort( byteOffset );  //Datum flotante en metros
+
+        printf("%d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\t %d\n", 
+ 	       seq_line, seq_file, cdp, cdp_seq, cdp_fold,
+	       offset, cdpx, cdpy, iln, xln, datum_pt, datum_p, datum_f, feof(fp));
+        trc++;
+        byteOffset = -1 * ((222)+ nm * 4 + 240);
+        fseek( fp,byteOffset, SEEK_CUR );
+        byteOffset = 0;
+      }
     }
     fclose(fp); 
   }
